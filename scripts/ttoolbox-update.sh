a0ea3115472e647e22fca6b2bf1c92bb26e68564
j12t <minhthanh.31oct2k3@gmail.com>
2026-06-20 13:15:33 +0100

    update scripts

--- scripts/ttoolbox-update.sh
+++ scripts/ttoolbox-update.sh
@@ -1,4 +1,4 @@
 #/bin/zsh
 #/bin/bash
 
-cd /opt/ttoolbox && git fetch origin && git reset --hard origin/main
+cd /opt/ttoolbox && git fetch origin && git reset --hard origin/main && chmod -r +x scripts/*
