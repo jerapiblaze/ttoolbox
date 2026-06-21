78062431a33cb8e632a655b159b1eab50b817a9a
j12t <minhthanh.31oct2k3@gmail.com>
2026-06-21 11:26:36 +0100

    update ping internet

--- scripts/pinginternet.sh
+++ scripts/pinginternet.sh
@@ -7,6 +7,7 @@ cn=$(nmcli -t --fields NAME --colors no --mode tabular c show --active 2>/dev/nu
 cd=$(nmcli -t --fields DEVICE --colors no --mode tabular c show --active 2>/dev/null | head -n 1)
 ca=$(nmcli -g IP4.ADDRESS device show $cd 2>/dev/null)
 cg=$(nmcli -g IP4.GATEWAY device show $cd 2>/dev/null)
+cq=$(nmcli -f IN-USE,SIGNAL,SSID device wifi | awk '/^\*/{if (NR!=1) {print $2}}')
 rs=$(curl -sL https://j12tee.qzz.io/ipcheck/json)
 
-echo "$ts; $cs; $cd; $cn; $ca; $cg; $rs"
+echo "$ts; $cs; $cd; $cn; $cq; $ca; $cg; $rs"
