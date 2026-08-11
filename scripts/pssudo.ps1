29e92a40828b481e48bb8b359eaba3d830228650
j12tee <tddt0310@gmail.com>
2026-08-11 23:11:25 +0100

    add pssudo wrapper

---
+++ scripts/pssudo.ps1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+    Wrapper around new sudo of windows to allow for elevation of privileges in PowerShell scripts.
+
+.DESCRIPTION
+    This script is designed to be used in conjunction with the `pssudo` module, which provides a way to run commands with elevated privileges.
+
+.EXAMPLE
+    pssudo.ps1 commands
+#>
+
+$command = $args -join " "
+Write-Output "Executing command with elevated privileges: $command"
+sudo powershell -c "$command"
