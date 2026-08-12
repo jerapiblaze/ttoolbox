5a961b564fa624454beb1b0e57dad2d5a9ac4ea8
j12tee <tddt0310@gmail.com>
2026-08-12 15:27:16 +0100

    small change to update script

--- scripts/ttoolbox-update.sh
+++ scripts/ttoolbox-update.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
 #!/usr/bin/env zsh
 
-cd /opt/ttoolbox && git fetch --depth=1 origin && git reset --hard origin/main && chmod +rx scripts/*
+# add -c flag to run check only mode
+
+# Get the current script dir
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ttoolbox_dir="$(cd "$script_dir/.." && pwd)"
+cd "$ttoolbox_dir" || exit 1
+
+CHECK_ONLY=false
+while getopts "c" opt; do
+    case $opt in
+        c)
+            CHECK_ONLY=true
+            ;;
+    esac
+done
+if [ "$CHECK_ONLY" = true ]; then
+    localCommit=$(git rev-parse HEAD)
+    remoteCommit=$(git rev-parse origin/main)
+    if [ "$localCommit" = "$remoteCommit" ]; then
+        printf "\033[33mNo updates available.\033[0m" # Yellow
+    else
+        printf "\033[32mUpdates are available. Run this script without -c flag to update.\033[0m" # Green
+    fi
+    exit 0
+fi
+git fetch --depth=1 origin && git reset --hard origin/main && chmod +rx scripts/*
