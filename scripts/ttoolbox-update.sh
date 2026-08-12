11fba9a3aa2a11cb5f3f29bc40553c1b2409b0bd
j12tee <tddt0310@gmail.com>
2026-08-12 15:27:59 +0100

    small fix to update script

--- scripts/ttoolbox-update.sh
+++ scripts/ttoolbox-update.sh
@@ -20,9 +20,9 @@ if [ "$CHECK_ONLY" = true ]; then
     localCommit=$(git rev-parse HEAD)
     remoteCommit=$(git rev-parse origin/main)
     if [ "$localCommit" = "$remoteCommit" ]; then
-        printf "\033[33mNo updates available.\033[0m" # Yellow
+        printf "\033[33mNo updates available.\n\033[0m" # Yellow
     else
-        printf "\033[32mUpdates are available. Run this script without -c flag to update.\033[0m" # Green
+        printf "\033[32mUpdates are available. Run this script without -c flag to update.\n\033[0m" # Green
     fi
     exit 0
 fi
