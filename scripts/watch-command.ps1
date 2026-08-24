0113b101c24ddc8d513d35a3c8b2f2f26922d270
j12tee <tddt0310@gmail.com>
2026-08-24 23:18:57 +0100

    enchance watch-commands.ps1

--- scripts/watch-command.ps1
+++ scripts/watch-command.ps1
@@ -33,21 +33,25 @@ if ($Command -eq "") {
 while ($true) {
     Clear-Host;
     # Banner: Every $Interval seconds: $Command <flexible white space> <current time>
-    $left_txt     = "Every $interval seconds: $command"
+    $left_txt     = "Every $Interval seconds: $Command"
     $right_txt    = (Get-Date).ToString("HH:mm:ss")
     $width_txt = $Host.UI.RawUI.WindowSize.Width
     $padding_txt = $width_txt - $left_txt.Length - $right_txt.Length
     if ($padding_txt -lt 1) { $padding_txt = 1 }
     if ($MaxLines -gt 0) {
-        $maxLines_txt = $MaxLines - 3   # leave room for header/footer if needed
+        $maxLines_txt = $MaxLines - 5   # leave room for header/footer if needed
     } else {
         $height_txt = $Host.UI.RawUI.WindowSize.Height
-        $maxLines_txt = $height_txt - 3
+        $maxLines_txt = $height_txt - 5
     }
     Write-Host "$left_txt$([string]::new(' ', $padding_txt))$right_txt" -BackgroundColor DarkBlue -ForegroundColor White
     $seperator_txt = "-" * $width_txt
     Write-Host $seperator_txt
-    $output_txt = (Invoke-Expression $command | Out-String) -split [Environment]::NewLine
+    $time_start = Get-Date
+    $output_txt = (Invoke-Expression $Command 2>&1 3>&1 4>&1 5>&1 6>&1 | Out-String)
+    $time_end = Get-Date
+    $execution_time = $time_end - $time_start
+    $output_txt = $output_txt -split [Environment]::NewLine
     if ($output_txt.Length -gt $maxLines_txt) {
         if ($Tail) {
             $output_txt = $output_txt[($output_txt.Length - $maxLines_txt)..($output_txt.Length - 1)]
@@ -56,10 +60,14 @@ while ($true) {
         }
         $output_txt = $output_txt -join [Environment]::NewLine
         Write-Host $output_txt
+        Write-Host $seperator_txt
         Write-Host "Output truncated to $maxLines_txt lines..." -ForegroundColor Yellow
+        Write-Host "Execution time: $($execution_time.TotalSeconds) seconds" -ForegroundColor Green
     } else {
         $output_txt = $output_txt -join [Environment]::NewLine
         Write-Host $output_txt
+        Write-Host $seperator_txt
+        Write-Host "Execution time: $($execution_time.TotalSeconds) seconds" -ForegroundColor Green
     }
     Start-Sleep -Seconds $Interval
 }
